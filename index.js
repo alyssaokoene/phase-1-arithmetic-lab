a8640eca8e2a6a92197d6ce00026feffb390ec68
Alyssa Okoene <alyssaokoenebusiness@gmail.com>
2023-04-02 08:47:56 -0500

    complete assignment arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 12;
+let num4 = 3;
+let mod = num3 / num4;
+
+let numbers = [1, 5, 2, 10, 8, 20];
+let max = Math.max(...numbers);
